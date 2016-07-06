63560eb5a8663a7dc3835208b79b8be918e0254e
MShultz <mshultz@student.neumont.edu>
2016-07-06 11:24:57 -0600

    Initial Commit. Method stubs created, and returns have invalid values.

---
+++ src/PersistentArray.java
@@ -0,0 +1,31 @@
+public class PersistentArray {
+
+	public PersistentArray(String arrayFileName) {
+
+	}
+
+	public static void initialize(String arrayFileName, int arraySize,
+			long initialValue) {
+
+	}
+
+	public void set(int index, long value) {
+
+	}
+
+	public long get(int index) {
+		return -1;
+	}
+
+	public long getLength() {
+		return -1;
+	}
+
+	public void close() {
+
+	}
+
+	public static void delete(String arrayFileName) {
+
+	}
+}
